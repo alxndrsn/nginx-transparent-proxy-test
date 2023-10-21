aa7eb8afb7a0bb971a31a7859df6c9099d6df61a
alxndrsn <alxndrsn>
2023-10-21 09:29:32 +0000

    Add direct request for comparison

--- test.js
+++ test.js
@@ -6,6 +6,20 @@ const fetchOpts = {
 };
 
 describe('nginx-transparent-redirect-proxy', () => {
+  describe('direct request to example.com (for comparison)', () => {
+    it('should return 200 with normal HTML', async () => {
+      // when
+      const res = await fetch('http://example.com', fetchOpts);
+
+      // then
+      assert.equal(res.status, '200');
+      assert.isNull(res.headers.get('location'));
+      // and
+      const body = await res.text();
+      assert.equal(body, exampleHtml());
+    });
+  });
+
   describe('express', () => {
     it('should redirect / with normal 307 headers', async () => {
       // when
