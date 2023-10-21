883b93d08af6ad946991b34e4c22ee2d078b3f08
alxndrsn <alxndrsn>
2023-10-21 09:27:34 +0000

    express: make path more specific

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
   res.redirect(307, 'http://example.com');
 });
 
-app.get('/download/*', (req, res) => {
+app.get('/download/some.file', (req, res) => {
   // use an example which actually exists :¬)
   res.redirect(307, 'http://example.com/index.html');
 });
